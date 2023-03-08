d0e6c4600108dc32c384e613a9de92f49eac886a
mauriciodioli <madioli26@hotmail.com>
2023-03-08 09:51:53 -0300

    modificaciones del miercoles

--- src/strategies/datoSheet.py
+++ src/strategies/datoSheet.py
@@ -2,6 +2,7 @@ from flask import Blueprint,render_template
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os #obtener el directorio de trabajo actual
 #import drive
 #drive.mount('/content/gdrive')
 
@@ -9,9 +10,29 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 datoSheet = Blueprint('datoSheet',__name__)
 
+SPREADSHEET_ID='1pyPq_2tZJncV3tqOWKaiR_3mt1hjchw12Bl_V8Leh74'#drpiBot2
 
-
-@datoSheet.route('/datosDesdeSheet/')
-def datosDesdeSheet():     
- return render_template('/estrategias.html')
+@datoSheet.route('/leerSheet/')
+def leerSheet():         
+     scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
     
+     newPath = os.path.join(os.getcwd(), 'strategies\\pruebasheetpython.json')  
+     print(newPath)
+     creds = ServiceAccountCredentials.from_json_keyfile_name(newPath, scope)
+     
+     #creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/mDioli/Documents/ITCOR/bot421/src/strategies/pruebasheetpython.json', scope)
+     client = gspread.authorize(creds)
+     
+     sheet = client.open_by_key(SPREADSHEET_ID).sheet1   
+     symbol = sheet.col_values(1)
+     trade_en_curso = sheet.col_values(19)
+     ut = sheet.col_values(20)
+     senial = sheet.col_values(21)
+     union = zip(symbol,trade_en_curso,ut,senial)
+     
+     for Symbol,trade_en_curso,ut,senial  in union:
+      print(Symbol,trade_en_curso,ut,senial)
+    
+     
+     return render_template('/estrategias.html')
