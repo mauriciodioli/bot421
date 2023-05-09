95bf83586a96fc85af63f2f4a31a28f34f158c7e
mauriciodioli <madioli26@hotmail.com>
2023-05-09 18:47:01 -0300

    martes

--- src/strategies/estrategiaSheetWS.py
+++ src/strategies/estrategiaSheetWS.py
@@ -120,12 +120,12 @@ def market_data_handler_estrategia( message):
         #print(f'El instrumento en market_data_handler {message}')
         # Llamando al método estrategiaSheet() desde market_data_handler_estrategia
      
-    print( "marca de tiempo:",  get.ahora)
+    print( "marca de tiempo:",  get.VariableParaTiemposMDHandler)
     marca_de_tiempo = message["timestamp"]
     #if  message["timestamp"] - ahora >= 600000: # 10 minutos
-    if  marca_de_tiempo - get.ahora >= 30000: # 30 segundos
-            print ( "ENTROOOO ahora:",  get.ahora  )
-            get.ahora = message["timestamp"]# milisegundos
+    if  marca_de_tiempo - get.VariableParaTiemposMDHandler >= 30000: # 30 segundos
+            print ( "ENTROOOO ahora:",  get.VariableParaTiemposMDHandler  )
+            get.VariableParaTiemposMDHandler = message["timestamp"]# milisegundos
             estrategiaSheetNuevaWS(message)
         
     
