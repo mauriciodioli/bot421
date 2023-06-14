c38f2b2e91124026e66661e12da878085aeb3c97
mauriciodioli <madioli26@hotmail.com>
2023-06-14 10:34:28 -0300

    modificaciones del miercoles

--- src/social/imagenes/imagenesOperaciones.py
+++ src/social/imagenes/imagenesOperaciones.py
@@ -12,17 +12,26 @@ def subirImagen():
 
 @imagenesOperaciones.route('/cargarImagen', methods=['POST'])
 def cargarImagen():
-    title = request.form['title']
-    description = request.form['description']
-    image = request.files['image']
-    filename = image.filename
+  
+    print("dfffffffffffffffffffffffaa")
+    title = request.form['numeroAleatorio']
+    nombreUsuario = request.form['nombreUsuario']
+    print("nombreUsuario___________",nombreUsuario,"_________title________",title)
+       # Obtener el archivo enviado por Dropzone
+   # archivo = request.files['file']
+
+    # Procesar el archivo como desees
+    # Por ejemplo, puedes guardarlo en el sistema de archivos
+   # archivo.save('/ruta/donde/guardar/el/archivo.jpg')
+   # image = request.files['image']
+   # filename = image.filename
     # Guardar la imagen en el servidor
-    filepath = os.path.join("carpeta_destino", filename)
-    image.save(filepath)
+   # filepath = os.path.join("carpeta_destino", filename)
+   # image.save(filepath)
     # Guardar la información de la imagen en la base de datos
-    new_image = Image(title=title, description=description, filepath=filepath)
-    db.session.add(new_image)
-    db.session.commit()
+   # new_image = Image(title=title, description=description, filepath=filepath)
+   # db.session.add(new_image)
+   # db.session.commit()
     return 'Image uploaded successfully'
 
 @imagenesOperaciones.route('/MostrarImages')
