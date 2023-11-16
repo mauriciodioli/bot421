6209bee3d49ac8264cf799fb123c4f141a239777
mauriciodioli <madioli26@hotmail.com>
2023-11-16 10:21:53 -0300

    jueves

--- src/social/imagenes/imagenesOperaciones.py
+++ src/social/imagenes/imagenesOperaciones.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask,jsonify, request, render_template, Blueprint
+from flask import Flask,jsonify, request, render_template, Blueprint,current_app, url_for
 from utils.db import db
 from models.modelMedia.image import Image
 
@@ -40,20 +40,34 @@ def cargarImagen():
       
         new_path = os.path.join( 'static', 'uploads', imagen.filename)
        # Guardar la imagen en la carpeta src/static/uploads
-        print(f"Ruta completa del archivo: {new_path}")
+       # print(f"Ruta completa del archivo: {new_path}")
         imagen.save(new_path)
         # Resto de tu lógica para manejar la imagen, el nombre del archivo y el access_token
         # Aquí puedes acceder a 'imagen' (objeto FileStorage), 'nombre_archivo' y 'access_token'
-
+        MostrarImages()
         return jsonify({'mensaje': 'Imagen cargada con éxito', 'nombreArchivo': nombre_archivo})
   except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@imagenesOperaciones.route('/MostrarImages')
+@imagenesOperaciones.route('/MostrarImages/')
 def MostrarImages():
     # Obtener las imágenes desde la base de datos
-    images = Image.query.all()
-    return render_template('images.html', images=images)
+    #images = Image.query.all()
+     # Obtener las rutas completas de las imágenes
+    #image_paths = [os.path.join('static', 'uploads', image.filename) for image in images]
+    
+   # Obtener la ruta completa de la carpeta 'static/uploads'
+    uploads_folder = os.path.join(current_app.root_path, 'static', 'uploads')
+
+    # Obtener todas las imágenes en la carpeta 'static/uploads'
+    image_files = [file for file in os.listdir(uploads_folder) if file.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+
+    # Crear las rutas completas de las imágenes sin codificación de caracteres
+    image_paths = [os.path.join('uploads', filename).replace(os.sep, '/') for filename in image_files]
+
+    return render_template('media/principalMedia/images.html', image_paths=image_paths)
+    
+  
 
 @imagenesOperaciones.route('/mostrarGaleria', methods = ['POST'])
 def MostrarGaleria():
