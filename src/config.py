a9159f8c872a17f412c68bb06097ea4ee5c631bb
Mauricio Dioli <madioli26@hotmail.com>
2024-01-22 13:50:35 +0000

    cambios para imagen

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ user = os.environ["MYSQL_USER"]
 password = os.environ["MYSQL_PASSWORD"]
 host = os.environ["MYSQL_HOST"]
 database = os.environ["MYSQL_DATABASE"]
+port = os.environ["MYSQL_PORT"]
 
-
-DATABASE_CONNECTION_URI = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+DATABASE_CONNECTION_URI = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
 #print(DATABASE_CONNECTION_URI)
