7da1770ae0f5afb62325e99c2df54d46c1e04bed
mauriciodioli <madioli26@hotmail.com>
2024-02-27 16:11:47 -0300

    agrega automatizacion

--- src/automatizacion/programar_trigger.py
+++ src/automatizacion/programar_trigger.py
@@ -177,13 +177,6 @@ def tarea_inicio(user_id,app,triggerEstrategias,usuario):
         # Procesar los usuarios (aquí puedes hacer lo que necesites con los resultados)
         
         for triggerEstrategia in triggerEstrategias:
-            print(triggerEstrategia.nombreEstrategia)  # Acceder al atributo 'id' de cada instancia de TriggerEstrategia
-            print(triggerEstrategia.horaInicio)
-            print(triggerEstrategia.horaFin)
-            print(triggerEstrategia.ManualAutomatico)
-            
-          
-
                 # Construir los datos a enviar a la otra ruta
             datos = {
                         "userCuenta": triggerEstrategia.userCuenta,
@@ -210,40 +203,57 @@ def tarea_inicio(user_id,app,triggerEstrategias,usuario):
             #response = requests.post(url_for(url), data=datos)
             # Construir la URL de destino
             url_destino = 'http://127.0.0.1:5001/' + triggerEstrategia.nombreEstrategia
-            print("**************************************************inicia***********************")
-            print("Tarea de inicio ejecutada")
-            print("**************************************************")
+            
+           
             # Enviar los datos a la estrategia
             response = requests.post(url_destino, json=datos)
 
             if response.status_code == 200:
-                    print("Datos de usuario enviados con éxito")
+                print("**************************************************inicia***********************")
+                print("Datos de usuario enviados con éxito")
+                print("estrategia ",triggerEstrategia.nombreEstrategia," iniciada con exito")
+                print("**************************************************")
             else:
-                    print("Error al enviar los datos de usuario")
+                    print("Error al enviar los datos de usuario en  automatizacion/programar_trigger/thread/tarea_inicio")
 
 
         
-def llama_tarea_cada_24_horas_estrategias(user_id,app):
+def llama_tarea_cada_24_horas_estrategias(user_id, app):
     get.hilo_iniciado_estrategia_usuario
     triggerEstrategias = db.session.query(TriggerEstrategia).all()
+    hilo_iniciado = False
     
     for triggerEstrategia in triggerEstrategias:
-     usuario = db.session.query(Usuario).filter_by(id=triggerEstrategia.user_id).first()
-     if usuario:
-        print("El usuario existe en la lista triggerEstrategias.")
-     
-        # Verifica si ya hay un hilo iniciado para este país
-        if user_id in get.hilo_iniciado_estrategia_usuario and get.hilo_iniciado_estrategia_usuario[user_id].is_alive():
-            return f"Hilo para {user_id} ya está en funcionamiento"
+        usuario = db.session.query(Usuario).filter_by(id=triggerEstrategia.user_id).first()
+        if usuario:
+            print("El usuario existe en la lista triggerEstrategias.")
+            
+            # Verifica si ya hay un hilo iniciado para este usuario
+            if user_id in get.hilo_iniciado_estrategia_usuario and get.hilo_iniciado_estrategia_usuario[user_id].is_alive():
+                print(f"Hilo para {user_id} ya está en funcionamiento para la estrategia {triggerEstrategia.nombreEstrategia}")
+                continue
+            
+            print("Hora de inicio:", triggerEstrategia.horaInicio)
+            
+            # Si no hay un hilo iniciado para este usuario, lo inicia
+            hilo = threading.Thread(target=ejecutar_tarea, args=(user_id, app, triggerEstrategias, usuario))
+            get.hilo_iniciado_panel_control[user_id] = hilo
+            hilo.start()
+            
+            # Programa la tarea para ejecutarse a la hora específica de la estrategia
+            schedule.every().day.at(triggerEstrategia.horaInicio.strftime('%H:%M:%S')).do(lee_trigger_ejecuta_estrategia, user_id, app, triggerEstrategias, usuario)
+            
+            hilo_iniciado = True
+            
+        else:
+            print("El usuario no existe en la lista triggerEstrategias.")  
 
-        # Si no hay un hilo iniciado para este país, lo inicia
-        hilo = threading.Thread(target=ejecutar_tarea, args=(user_id,app,triggerEstrategias,usuario))
-        get.hilo_iniciado_panel_control[user_id] = hilo
-        hilo.start()
-     else:
-        print("El usuario no existe en la lista triggerEstrategias.")  
+    if hilo_iniciado:
+        return f"Hilo(s) iniciado(s) para {user_id}"
+    else:
+        return f"No se inició ningún hilo para {user_id}"
 
-    return f"Hilo iniciado para {user_id}"
+
 
 def ejecutar_tarea(user_id,app,triggerEstrategias,usuario):
     #if get.ya_ejecutado_hilo_panelControl == False:
