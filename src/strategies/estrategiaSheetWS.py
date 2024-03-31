e0f40428711ae9e9a814d2ace63184b1118bd9ac
Mauricio Dioli <madioli26@hotmail.com>
2024-03-31 14:18:05 +0000

    acceso automatico

--- src/strategies/estrategiaSheetWS.py
+++ src/strategies/estrategiaSheetWS.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash,jsonify,g
+from flask import Blueprint, render_template,session, request, redirect, url_for, flash,jsonify,g
 import routes.instrumentosGet as instrumentosGet
 from utils.db import db
 from models.orden import Orden
 from models.usuario import Usuario
+import os
 import re
 import jwt
 import csv
@@ -209,7 +210,9 @@ def get_instrumento_para_suscripcion_db(app):
 
 def get_instrumento_para_suscripcion_json():
    try:
-        ruta_archivo_json = 'strategies/listadoInstrumentos/instrumentos_001.json'    
+        src_directory = os.getcwd() # Busca directorio raíz src o app 
+        ruta_archivo_json = os.path.join(src_directory, 'src/strategies/listadoInstrumentos/instrumentos_001.json')
+       # ruta_archivo_json = 'strategies/listadoInstrumentos/instrumentos_001.json'    
         with open(ruta_archivo_json , 'r') as archivo:
             contenido = archivo.read()
             datos = json.loads(contenido)
