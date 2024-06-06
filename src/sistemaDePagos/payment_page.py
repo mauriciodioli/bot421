ea93db0c4e353273fb88fe2528965a25428c66bd
mauriciodioli <madioli26@hotmail.com>
2024-06-06 11:27:20 -0300

    integracion mercado pago

--- src/sistemaDePagos/payment_page.py
+++ src/sistemaDePagos/payment_page.py
@@ -14,28 +14,78 @@ import tokens.token as Token
 import jwt
 from models.usuario import Usuario
 from models.brokers import Broker
-#from mercadopago import MercadoPago
+import mercadopago
+import asyncio
+import httpx
+
 
 # Inicializar la instancia de MercadoPago con tus credenciales
 #mp = MercadoPago("CLIENT_ID", "CLIENT_SECRET")
 
 payment_page = Blueprint('payment_page',__name__)
 
-
-YOUR_ACCESS_TOKEN = 'TEST-7897622499833241-051414-fa868bef7e053a323b97ebbd953bf95b-630055'
+sdk = "APP_USR-5717567227383881-060409-1e8fafe5e95b1bc7f5b5c9a86dc40999-1835443126" #de produccion en el usuario vendedor de prueba
+YOUR_ACCESS_TOKEN = 'TEST-7897622499833241-051414-fa868bef7e053a323b97ebbd953bf95b-630055' #de produccion
 preapproval_plan_id = 'YOUR_PREAPPROVAL_PLAN_ID'
 
 
+mp = mercadopago.SDK(YOUR_ACCESS_TOKEN)
 
-
-@payment_page.route('/pago', methods=['GET'])
-def pago():
-    # Obtener los valores de los inputs ocultos
-    costo_base = request.args.get('costo_base')
-    porcentaje_retorno = request.args.get('porcentaje_retorno')
+def obtener_preference_id():
+    # Configurar el SDK de Mercado Pago con tu clave pública
+    global mp
+    # Crear un objeto de preferencia de pago
+    preference = {
+        "items": [
+            {
+                "title": "Producto de ejemplo",
+                "quantity": 1,
+                "currency_id": "ARS",  # Moneda (ARS para Argentina)
+                "unit_price": 1000  # Precio en centavos
+            }
+        ]
+    }
     
-    # Convertir los valores a números flotantes
+    # Crear la preferencia en Mercado Pago
+    preference_response = mp.preference().create(preference)   
+    preference_result = preference_response["response"]
+
+    # Obtener el ID de preferencia de la respuesta
+    preference_id = preference_result['id']
+
+    return preference_id
+
+@payment_page.route('/crear_preferencia', methods=['POST'])
+def crear_preferencia():
+    
+    preference_id = obtener_preference_id()
+    return jsonify({'preference_id': preference_id})
+
+@payment_page.route('/pagoMercadoPago/', methods=['POST'])
+def pagoMercadoPago():
     try:
+        # Obtén los datos del formulario HTML
+        costo_base = float(request.form.get("costo_base", 0))
+        porcentaje_retorno = float(request.form.get("porcentaje_retorno", 0))
+        
+        # Calcula el total
+        total = costo_base * (1 + porcentaje_retorno / 100)
+        
+        # Renderiza la plantilla "mercadoPago.html" y pasa los datos como argumentos
+        return render_template('sistemaDePagos/mercadoPago.html', costo_base=costo_base, porcentaje_retorno=porcentaje_retorno, total=total)
+    
+    except (ValueError, TypeError):
+        return jsonify({"error": "Invalid input data"}), 400
+
+@payment_page.route('/pago/', methods=['POST'])
+def pago():
+     # Convertir los valores a números flotantes
+    try:
+        data = request.form
+        costo_base = int(data['costo_base'])
+        porcentaje_retorno = float(data['porcentaje_retorno'])
+    
+   
         costo_base = float(costo_base)
         porcentaje_retorno = float(porcentaje_retorno)
     except (ValueError, TypeError):
@@ -47,79 +97,139 @@ def pago():
    # total = costo_base * (1 + porcentaje_retorno / 100)
     total = costo_base
     # Renderizar la plantilla y pasar los valores como argumentos
-    return render_template('sistemaDePagos/payment_page.html', costo_base=costo_base, porcentaje_retorno=porcentaje_retorno, total=total)
-
+  
+    return jsonify({"init_point": 'sistemaDePagos/mercadoPago.html'})
 
 @payment_page.route('/process_payment', methods=['POST'])
 def process_payment():
     try:
-        # Aquí se manejaría la lógica de procesamiento de pagos
-        # Por ejemplo, integrar con una API de pasarela de pagos
         data = request.json
-        card_number = data.get('card_number')
-        amount = data.get('amount')
-       # Crear un pago
         payment_data = {
-            "transaction_amount": 100,
-            "reason": "Compra de producto",
+            "transaction_amount": data['transaction_amount'],
+            "token": data['token'],
+            "description": data['description'],
+            "payment_method_id": data['payment_method_id'],
+            "installments": data['installments'],
             "payer": {
-                "email": "test_user_123456@testuser.com"
-            },
-            "payment_method_id": "visa",
-            "installments": 1
+                "email": data['payer']['email']
+            }
         }
-        #payment_response = mp.post("/v1/payments", payment_data)
-        payment_response = 'vacio'
-        # Verificar si el pago fue aprobado
-        if payment_response["status"] == 201:
-            print("El pago fue exitoso.")
-        else:
-            print("El pago falló.")
-        # Respuesta simulada para el ejemplo
-        response = {
-            'status': 'success',
-            'message': f'Payment of {amount} ARS with card ending {card_number[-4:]} processed successfully.'
-        }
-        return jsonify(response)
+        result = mp.payment().create(payment_data)
+        payment = result["response"]
+        return jsonify(payment)
     except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
-    
+        return jsonify({'error': str(e)})
+
 payment_page.route('/producto/<int:producto_id>')
 def producto(producto_id):
     return render_template(f'producto{producto_id}.html')
 
+
+async def create_preference(sdk, preference_data, headers):
+    async with httpx.AsyncClient() as client:
+        url = "https://api.mercadopago.com/checkout/preferences"
+        response = await client.post(
+            url,
+            json=preference_data,
+            headers=headers
+        )
+        response.raise_for_status()
+        return response.json()
+     
+
 @payment_page.route('/create_order/', methods=['POST'])
-def create_order():
+async def create_order():
     try:
-        data = request.json
-        card_number = data.get('card_number')
-        amount = data.get('amount')
+        # Obtén los datos de la solicitud
+        data = request.get_json()
+
+        # Extrae los valores necesarios
+        costo_base = data.get("items")[0].get("unit_price")
+        porcentaje_retorno = request.form.get("porcentaje_retorno", 0)
+        costo_base = float(costo_base)
+        # Crea los datos de la preferencia
+        preference_data = {
+            "items": [
+                {
+                    "title": "Donacion de prueba",  # Título del artículo
+                    "quantity": 1,
+                    "unit_price": costo_base,  # Precio unitario
+                    "currency_id": "ARS"
+                }
+            ],
+            "back_urls": {
+                "success": "https://89ae-190-225-182-66.ngrok-free.app/success",
+                "failure": "https://89ae-190-225-182-66.ngrok-free.app/failure",
+                "pending": "https://89ae-190-225-182-66.ngrok-free.app/pending"
+            },
+             "auto_return": "approved"
+        }
+
+       # Encabezados para la solicitud
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + sdk  # Token de acceso
+        }
+
+        # Llama a la función create_preference
+        preference_response = await create_preference(sdk, preference_data, headers)
+        init_point = preference_response.get("init_point")
+       
+        # Devuelve la URL de inicialización de la preferencia
+        return jsonify({"init_point": init_point})
+
+    except httpx.HTTPStatusError as e:
+        return jsonify({"error": str(e)}), e.response.status_code
+
+
+    
+@payment_page.route('/success/', methods=['GET'])
+async def success():
+    try:
+        payment_id = request.args.get('collection_id')
+        status = request.args.get('collection_status')
+        external_reference = request.args.get('external_reference')
+        payment_type = request.args.get('payment_type')
+        
         response = {
             'status': 'success',
-            'message': f'Payment of {amount} ARS with card ending {card_number[-4:]} processed successfully.'
+            'payment_id': payment_id,
+            'status': status,
+            'external_reference': external_reference,
+            'payment_type': payment_type
         }
         return jsonify(response)
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
-    
-@payment_page.route('/success_payment/', methods=['POST'])
-def success_payment():
+
+@payment_page.route('/webhook/', methods=['POST'])
+async def webhook():
     try:
-        data = request.json
-        card_number = data.get('card_number')
-        amount = data.get('amount')
-        response = ''
+        data = await request.get_json()
+        # Procesa los datos del webhook según tus necesidades
+        print("Webhook received:", data)
+        # Aquí puedes manejar la lógica de procesamiento de los datos recibidos
+        
+        return jsonify({'status': 'received'}), 200
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+    
+@payment_page.route('/failure/', methods=['GET'])
+def failure():
+    try:
+        card_number = request.args.get('card_number')
+        amount = request.args.get('amount')
+        response = {'status': 'failure', 'message': 'Payment failed', 'card_number': card_number, 'amount': amount}
         return jsonify(response)
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
-
-@payment_page.route('/webhock_success_payment/', methods=['POST'])
-def webhock_success_payment():
+    
+@payment_page.route('/pending/', methods=['GET'])
+def pending():
     try:
-        data = request.json
-        card_number = data.get('card_number')
-        amount = data.get('amount')
-        response = ''
+        card_number = request.args.get('card_number')
+        amount = request.args.get('amount')
+        response = {'status': 'pending', 'message': 'Payment pending', 'card_number': card_number, 'amount': amount}
         return jsonify(response)
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
