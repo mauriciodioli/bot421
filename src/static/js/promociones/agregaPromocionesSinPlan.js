c24fb93b48f32f4635fe812b019055bc516b892b
Mauricio Dioli <madioli26@hotmail.com>
2024-07-06 17:34:07 +0000

    modifacacion promociones

--- src/static/js/promociones/agregaPromocionesSinPlan.js
+++ src/static/js/promociones/agregaPromocionesSinPlan.js
@@ -6,11 +6,41 @@ $(document).ready(function() {
             descuento: $('#promotionDiscount').val(),
             razon: $('#promotionReason').val(),
             estado: $('#promotionStatus').val(),         
-            moneda: $('#promotionCurrency').val()
+            moneda: $('#promotionCurrency').val(),
+            cluster: $('#promotionCluster').val()
         };
 
         $.ajax({
             url: '/productosComerciales_promociones_agrega_promocionesSinPlan',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(promotionData),
+            success: function(response) {
+                alert('Promoción agregada exitosamente');
+                $('#modalAgregarPromocionSinPlan').modal('hide');
+            },
+            error: function(error) {
+                alert('Error al agregar promoción');
+            }
+        });
+    });
+});
+
+
+$(document).ready(function() {
+    $('#savePromotionBtnAgregarPromocion').click(function() {
+        const promotionData = {
+            precio: $('#promotionPrice').val(),
+            descripcion: $('#promotionDescription').val(),
+            descuento: $('#promotionDiscount').val(),
+            razon: $('#promotionReason').val(),
+            estado: $('#promotionStatus').val(),         
+            moneda: $('#promotionCurrency').val(),
+            cluster: $('#promotionCluster').val()
+        };
+
+        $.ajax({
+            url: '/productosComerciales_promociones_modifica_promocionesSinPlan',
             method: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(promotionData),
