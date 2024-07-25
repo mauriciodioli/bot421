fbdd6e1f4b7d12ec19357d33f0fb4746cf038d32
mauriciodioli <madioli26@hotmail.com>
2024-07-25 12:44:39 -0300

    descomentado en estrategia y app

--- src/strategies/opera_estrategias.py
+++ src/strategies/opera_estrategias.py
@@ -125,7 +125,7 @@ def OperacionWs(pyRofexInicializada, diccionario_global_operaciones,diccionario_
                     user_id = diccionario_global_operaciones[Symbol]['user_id']
                     userCuenta = diccionario_global_operaciones[Symbol]['userCuenta']
                     accountCuenta = diccionario_global_operaciones[Symbol]['accountCuenta']
-                    #pyRofexInicializada.send_order_via_websocket(ticker=Symbol,size=ut,side=pyRofexInicializada.Side.BUY,order_type=pyRofexInicializada.OrderType.LIMIT,ws_client_order_id=_ws_client_order_id,price=precio,environment=accountCuenta)
+                    pyRofexInicializada.send_order_via_websocket(ticker=Symbol,size=ut,side=pyRofexInicializada.Side.BUY,order_type=pyRofexInicializada.OrderType.LIMIT,ws_client_order_id=_ws_client_order_id,price=precio,environment=accountCuenta)
 
                     ws_client_order_id = _ws_client_order_id
                         
@@ -173,7 +173,7 @@ def OperacionWs(pyRofexInicializada, diccionario_global_operaciones,diccionario_
                     userCuenta = diccionario_global_operaciones[Symbol]['userCuenta']
                     accountCuenta = diccionario_global_operaciones[Symbol]['accountCuenta']
                     print('operaciones Symbol ',Symbol, 'closed.')
-                   # pyRofexInicializada.send_order_via_websocket(ticker=Symbol,size=ut,side=pyRofexInicializada.Side.SELL,order_type=pyRofexInicializada.OrderType.LIMIT,ws_client_order_id=_ws_client_order_id,price=precio,environment=accountCuenta)
+                    pyRofexInicializada.send_order_via_websocket(ticker=Symbol,size=ut,side=pyRofexInicializada.Side.SELL,order_type=pyRofexInicializada.OrderType.LIMIT,ws_client_order_id=_ws_client_order_id,price=precio,environment=accountCuenta)
                     ws_client_order_id = _ws_client_order_id
                     
                     
