65b4ee424a1e649832b8857d701c81b653daf0cb
mauriciodioli <madioli26@hotmail.com>
2024-08-09 11:19:27 -0300

    configuracion de db y publicaciones

--- src/models/modelMedia/image.py
+++ src/models/modelMedia/image.py
@@ -12,16 +12,18 @@ image = Blueprint('image', __name__)
 class Image(db.Model):
     __tablename__ = 'image'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('usuarios.id'))  
+    user_id = Column(Integer, ForeignKey('usuarios.id'))
     title = db.Column(db.String(255), unique=True, nullable=False)
     description = db.Column(db.String(255), nullable=False)   
     colorDescription = db.Column(db.String(255), nullable=False) 
     filepath = db.Column(db.String(500), nullable=True)
-    randomNumber = db.Column(db.Integer) 
+    randomNumber = db.Column(db.Integer)
     
+    # Relación con Public_imagen_video
+    #publicaciones_imagen = relationship("Public_imagen_video", back_populates="imagen")
     usuarios = relationship("Usuario", back_populates="imagenes")
 
-    def __init__(self, user_id, title, description, filepath, randomNumber,colorDescription):
+    def __init__(self, user_id, title, description, filepath, randomNumber, colorDescription):
         self.user_id = user_id
         self.title = title
         self.description = description
@@ -29,18 +31,19 @@ class Image(db.Model):
         self.randomNumber = randomNumber
         self.colorDescription = colorDescription
 
+   
     def __repr__(self):
-        return f"Image(id={self.id}, user_id={self.user_id}, title={self.title}, description={self.description}, filepath={self.filepath}, randomNumber={self.randomNumber},colorDescription={self.colorDescription})"
+        return f"Imagen(id={self.id}, user_id={self.user_id}, title={self.title}, description={self.description}, filepath={self.filepath}, randomNumber={self.randomNumber}, colorDescription={self.colorDescription})"
 
     @classmethod
     def crear_tabla_image(cls):
         insp = inspect(db.engine)
         if not insp.has_table("image"):
-            db.create_all()
+            cls.__table__.create(db.engine)
 
 class MerShema(ma.Schema):
     class Meta:
-        fields = ("id", "user_id", "title", "description", "filepath", "randomNumber","colorDescription")
+        fields = ("id", "user_id", "title", "description", "filepath", "randomNumber", "colorDescription")
 
 mer_schema = MerShema()
 mer_shema = MerShema(many=True)
--- src/models/publicaciones/publicacion_imagen_video.py
+++ src/models/publicaciones/publicacion_imagen_video.py
@@ -4,52 +4,43 @@ from utils.db import db
 from sqlalchemy import inspect, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-
-
 ma = Marshmallow()
 
 publicacion_imagen_video = Blueprint('publicacion_imagen_video', __name__)
 
-
 class Public_imagen_video(db.Model):
     __tablename__ = 'publicacion_imagen_video'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    publicacion_id = Column(Integer, ForeignKey('usuarios.id'))   
-    imagen_id = db.Column(db.Integer, db.ForeignKey('image.id'))
-    video_id = db.Column(db.Integer, db.ForeignKey('video.id'))    
+    publicacion_id = db.Column(db.Integer,nullable=False)
+    imagen_id = db.Column(db.Integer,nullable=False)
+    video_id = db.Column(db.Integer,nullable=False)
     fecha_creacion = db.Column(db.DateTime)
-    imagenes = relationship("Image", back_populates="publicaciones")
-    usuarios = relationship("Usuario", back_populates="cuentas")
-    publicacion = relationship("Public_imagen_video", back_populates="publicaciones")
     
-   
-   
+    # Relaciones
+    #publicacion = relationship("Publicacion", back_populates="publicacion_imagen_video")
+    #imagen = relationship("Imagen", back_populates="publicaciones_imagen")
+    #video = relationship("Video", back_populates="publicaciones_video")
 
-    # constructor
-    def __init__(self,publicacion_id, imagen_id, video_id,fecha_creacion):
+  
+
+    def __init__(self, publicacion_id, imagen_id, video_id, fecha_creacion):
         self.publicacion_id = publicacion_id
         self.imagen_id = imagen_id
         self.video_id = video_id        
         self.fecha_creacion = fecha_creacion
-        
 
     def __repr__(self):
-        return f"Public_imagen_video(id={self.id}, imagen_id={self.imagen_id}, video_id={self.video_id}, fecha_creacion={self.fecha_creacion})"
+        return f"Public_imagen_video(id={self.id}, publicacion_id={self.publicacion_id}, imagen_id={self.imagen_id}, video_id={self.video_id}, fecha_creacion={self.fecha_creacion})"
 
     @classmethod
     def crear_tabla_Public_imagen_video(cls):
         insp = inspect(db.engine)
         if not insp.has_table("publicacion_imagen_video"):
-            db.create_all()
+            cls.__table__.create(db.engine)
             
-            
-  
-        
 class MerShema(ma.Schema):
     class Meta:
-        fields = ("id", "publicacion_id", "imagen_id", "video_id","fecha_creacion")
-
+        fields = ("id", "publicacion_id", "imagen_id", "video_id", "fecha_creacion")
 
 mer_schema = MerShema()
 mer_shema = MerShema(many=True)
-
