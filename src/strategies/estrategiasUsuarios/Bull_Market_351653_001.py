f3af3a59432d31e4c05012e4b29bbd959e8eef34
mauriciodioli <madioli26@hotmail.com>
2024-08-29 12:26:21 -0300

    a test

--- src/strategies/estrategiasUsuarios/Bull_Market_351653_001.py
+++ src/strategies/estrategiasUsuarios/Bull_Market_351653_001.py
@@ -939,7 +939,7 @@ def endingOperacionBot(endingGlobal, endingEnviadas, symbol):#terminacion
         if symbol in diccionario_global_operaciones and diccionario_operaciones_enviadas:
             print('endingGlobal___ ', endingGlobal, ' endingEnviadas', endingEnviadas, 'symbol: ', symbol)
             # Limpiar el diccionario si se cumplen todas las condiciones
-            diccionario_operaciones_enviadas.clear()
+           # diccionario_operaciones_enviadas.clear()
             print("###############################################") 
             print("###############################################") 
             print("###############################################")  
@@ -950,7 +950,7 @@ def endingOperacionBot(endingGlobal, endingEnviadas, symbol):#terminacion
             account = diccionario_global_operaciones[symbol]['accountCuenta']
             pyRofexInicializada = get.ConexionesBroker[account]['pyRofex'] 
             
-            pyRofexInicializada.remove_websocket_market_data_handler(market_data_handler_estrategia, environment=account)
+           # pyRofexInicializada.remove_websocket_market_data_handler(market_data_handler_estrategia, environment=account)
             estrategias_usuario_nadmin_desde_endingOperacionBot(get.ConexionesBroker[account]['cuenta'], idUser)
           
     except KeyError as e:
