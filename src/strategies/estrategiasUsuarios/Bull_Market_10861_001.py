e349eb5cbda5c0dc0b24fe51509b75318cda82d0
mauriciodioli <madioli26@hotmail.com>
2024-09-18 09:18:55 -0300

    test

--- src/strategies/estrategiasUsuarios/Bull_Market_10861_001.py
+++ src/strategies/estrategiasUsuarios/Bull_Market_10861_001.py
@@ -978,7 +978,7 @@ def endingOperacionBot(endingGlobal, endingEnviadas, symbol):
                 'account': get.ConexionesBroker[account]['cuenta'], 
                 'user_id': idUser, 
                 'symbol': symbol,
-                'mesaje' : 'FELICIDADES, EL BOT TERMINO DE OPERAR CON EXITO !!!',
+                'mensaje' : 'FELICIDADES, EL BOT TERMINO DE OPERAR CON EXITO !!!',
                 'status': 'termino'               
             }
 
