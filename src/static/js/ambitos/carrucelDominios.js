f79801ad141d514c5a7bc2fed9caaf68ab8d0441
Mauricio Dioli <madioli26@hotmail.com>
2025-01-27 05:58:58 -0300

    carrucel completo

--- src/static/js/ambitos/carrucelDominios.js
+++ src/static/js/ambitos/carrucelDominios.js
@@ -1,4 +1,68 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+  window.cargarAmbitos(); // Llamar a la función cuando el DOM esté listo
+});
+
+window.cargarAmbitos = function () {
+  fetch('/social-media-publicaciones-obtener-ambitos', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+      }
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Error al obtener los ámbitos.');
+      }
+      return response.json();
+  })
+  .then(data => {
+    
+      // Obtener el contenedor donde se agregará el carrusel
+      const carruselContainer = document.querySelector('.carrusel-container');
+      
+      // Limpiar el contenedor en caso de que ya haya elementos
+      carruselContainer.innerHTML = '';
+      
+      // Iterar sobre los datos y crear los items del carrusel con los valores 'nombre' y 'valor'
+      data.forEach(item => {
+          const divItem = document.createElement('div');
+          divItem.classList.add('carrusel-item');
+          divItem.textContent = item.nombre;  // Usar 'nombre' para mostrar en el carrusel
+          divItem.setAttribute('data-valor', item.valor);  // Almacenar 'valor' en un atributo para su uso posterior
+          carruselContainer.appendChild(divItem);
+      });
+
+      // Obtener todos los elementos del carrusel para añadirles el evento de clic
+      const items = document.querySelectorAll('.carrusel-item');
+
+      // Añadir el event listener para cada item
+      items.forEach(item => {
+          item.addEventListener('click', () => {
+              // Elimina la clase 'active' de todos los items
+              items.forEach(i => i.classList.remove('active'));
+              
+              // Añadir la clase 'active' al item en el que se ha hecho clic
+              item.classList.add('active');
+
+              // Obtener el valor almacenado en el atributo 'data-valor'
+              const valor = item.getAttribute('data-valor');
+              console.log('Valor del item seleccionado:', valor);
+              
+              // Modificar el localStorage con el nuevo valor seleccionado
+              localStorage.setItem('dominio', valor);
+               // Llamar a la función de enviarDominioAJAX desde dpi.js
+               enviarDominioAJAX(valor);  // Pasar el valor a la función AJAX
+          });
+      });
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
+};
+
+
+
 
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
